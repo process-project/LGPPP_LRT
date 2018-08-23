ec65d9980061fe4c21e8eff27d1581ed915e8e2a
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2018-08-23 14:52:30 +0200

    Added setup.py to make the package pip installable, albeit from a private Bitbucket repo

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+    name='Reduction of archived LOFAR observations using LGPPP_LRT',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['LGPPP_LRT']                  # The name of your scipt, and also the command you'll be using for calling it
+)
