777aeb1b821317a8039792b7bae0d7cf49684cb6
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2018-08-24 13:48:06 +0200

    This is a minimal setup.py that actually installs LGPPP_LRT using 'pip install .'

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
  
 setup(
-    name='Reduction of archived LOFAR observations using LGPPP_LRT',    # This is the name of your PyPI-package.
+    name='LGPPP_LRT',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
-    scripts=['LGPPP_LRT']                  # The name of your scipt, and also the command you'll be using for calling it
 )
